712b229ac808fd366fb817c459183aa8f7557ce0
John Flournoy <john_flournoy@fas.harvard.edu>
2023-02-23 16:56:33 -0500

    rm

--- group_level_vwise/CR_m_Go/SwE_contrast_job_run_CR_m_Go.m
+++
@@ -1,7 +0,0 @@
-addpath('/ncf/mclaughlin/users/jflournoy/code/spm12')
-% List of open inputs
-jobfile = {'/ncf/mclaughlin/users/jflournoy/code/hcpd_tfMRI/group_level/CR_m_Go/SwE_contrast_job_CR_m_Go.m'};
-jobs = repmat(jobfile, 1, 1);
-inputs = cell(0, 1);
-spm('defaults', 'FMRI');
-spm_jobman('run', jobs, inputs{:});
